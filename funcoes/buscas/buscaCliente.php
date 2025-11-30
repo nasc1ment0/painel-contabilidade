4f51e0b14fc7bfa74fa5f9759920e948d0fe166f
Henrique Nascimento <nehenrique16@gmail.com>
2025-11-30 20:29:30 -0300

    Alteração na busca para pegar apenas clientes ativos

--- funcoes/buscas/buscaCliente.php
+++ funcoes/buscas/buscaCliente.php
@@ -9,7 +9,7 @@ $resultado = [];
 
 if (strlen($termo) >= 2) {
     $clientes = $db->getRegistros("SELECT id_cliente, nm_cliente, nr_documento FROM tb_clientes 
-        WHERE (nm_cliente LIKE :termo OR nr_documento LIKE :termo) ORDER BY nm_cliente LIMIT 10", [":termo" => "%$termo%"]);
+        WHERE (nm_cliente LIKE :termo OR nr_documento LIKE :termo) AND ativo = 'S' ORDER BY nm_cliente LIMIT 10", [":termo" => "%$termo%"]);
 
     foreach ($clientes as $c) {
         $resultado[] = [
